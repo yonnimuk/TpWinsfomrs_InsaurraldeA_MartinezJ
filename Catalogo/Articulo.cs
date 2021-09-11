22765ed394dd740dcce4d2dae96e2aab56872e1e
Jonathan Martinez <yonni.muk@gmail.com>
2021-09-11 20:47:05 -0300

    Primer commit

---
+++ Catalogo/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }
+        public string ImagenUrl { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}
